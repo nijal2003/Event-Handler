11638ac9f504abfb35dcb56a659e793a3d0e049c
nijal2003 <nijalghodasara2003@gmail.com>
2024-03-02 10:46:12 +0530

    Adding routes and form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Admin from './screens/Admin';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+// import Navbar from './components/Navbar';
+import Login from './screens/Login';
+import User from './screens/User';
+import Singup from './screens/Singup';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div>
+          <Routes>
+            <Route exact path="/" element={<Login />} />
+            <Route exact path="/admin" element={<Admin />} />
+            <Route exact path="/user" element={<User />} />
+            <Route exact path="/singup" element={<Singup />} />
+          </Routes>
+        </div>
+      </Router>
   );
 }
 
