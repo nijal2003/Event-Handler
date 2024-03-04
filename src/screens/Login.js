cdf3797feabcd020ddd073485af058d898909ab1
nijal2003 <nijalghodasara2003@gmail.com>
2024-03-04 23:00:02 +0530

    Customize routes

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
 
 export default function Login() {
+
+  const navigate = useNavigate();
+
+
+  const handleAdmin=(e)=>{
+    navigate(`/admin`)
+  }
+
+  const handleUser=(e)=>{
+    navigate(`/user`)
+  }
   return (
-    <div>Login</div>
+    <div>
+      <button onClick={handleAdmin}>Admin</button>
+      <button onClick={handleUser}>User</button>
+    </div>
   )
 }
